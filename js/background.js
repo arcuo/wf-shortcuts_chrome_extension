6a4c5b9d73cfff88c0b4d6c4624756d38942728e
Benjamin Zachariae <benjamin.zachariae@gmail.com>
2018-10-08 22:36:38 +0200

    added switch user feature

--- js/background.js
+++ js/background.js
@@ -3,16 +3,20 @@ All the shortcut command calls.
  */
 //TODO: Make work with stage and test.
 //TODO: make options page to change shortcuts.
-//TODO: make check for switch user work.
 //TODO: Check for deleting content (also from manifest).
 
 let getFlowID = (url) => {
     let re = RegExp("\\?id=(.*)", "g");
-    return re.exec(url)[1];
+    let flowId = re.exec(url);
+    if (flowId === null) {
+        return "ID missing";
+    } else {
+        return flowId[1];
+    }
 };
 
 let getBranchName = (url) => {
-    let re = RegExp("europe?-(.*)\.wiseflow")
+    let re = RegExp("europe?-(.*)\.wiseflow");
     return re.exec(url)[1];
 };
 
@@ -20,13 +24,55 @@ let searchForGoSwitchBack = () => {
     return document.querySelector('a[href="/index.php?switchUser=true"]')
 };
 
-function commandHandler(command, url, id) {
+function commandHandler(command, url, id, flowId) {
 
-    if (command == "to-manager-page") {
+    if (flowId === "ID missing") {
+        console.log(flowId);
+        return;
+    }
+
+    if (command === "to-manager-page") {
+
         chrome.tabs.update(
             id,
-            {url: "https://europe.wiseflow.net/manager/display.php?id=" + getFlowID(url)}
+            {url: "https://europe.wiseflow.net/manager/display.php?id=" + flowId}
         )
+
+    } else if (command === "to-manager-page-original") {
+
+        console.log("here1")
+
+        // Check for switch back user element and go back if necessary
+        chrome.tabs.executeScript(id, {
+            code: '(' + searchForGoSwitchBack + ')();'
+        }, (result) => {
+            console.log("here2")
+            console.log(result[0] !== null)
+            if (result[0] !== null) {
+                console.log("here3")
+                chrome.tabs.update(
+                    id,
+                    {url: "https://europe.wiseflow.net/index.php?switchUser=true"}
+                )
+
+                let listener = function(tabId, info) {
+                    if (info.status === 'complete' && tabId === id) {
+                        chrome.tabs.onUpdated.removeListener(listener);
+                        chrome.tabs.update(
+                            id,
+                            {url: "https://europe.wiseflow.net/manager/display.php?id=" + flowId}
+                        )
+                    }
+                }
+
+                chrome.tabs.onUpdated.addListener(listener);
+            } else {
+                chrome.tabs.update(
+                    id,
+                    {url: "https://europe.wiseflow.net/manager/display.php?id=" + flowId}
+                )
+            }
+        });
     }
 }
 
@@ -41,14 +87,14 @@ chrome.commands.onCommand.addListener(function (command) {
         let url = tab.url;
         let id = tab.id;
 
-        chrome.tabs.executeScript(id, {
-            code: '(' + searchForGoSwitchBack + ')();'
-        }, (result) => {
-            console.log(result)
-            console.log(result.length > 0)
-        })
+        if (url.search("wiseflow.net") === -1) {
+            console.log("Not WISEflow site.");
+            return;
+        }
 
-        commandHandler(command, url, id)
+        let flowId = getFlowID(url);
+
+        commandHandler(command, url, id, flowId)
 
     });
 
