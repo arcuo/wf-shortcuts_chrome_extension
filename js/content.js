069e001f7f921bc44d2843d89ab7de70f9549c0a
benjamin <arcuo>
2020-09-21 15:22:43 +0200

    removed content.js as it was unnecessary

--- js/content.js
+++
@@ -1,9 +0,0 @@
-// Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
-    if (msg.text === 'check-real-user') {
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
-        sendResponse(document.body);
-    }
-});
